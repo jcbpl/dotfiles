92ba6f2bdd7f3fa5905a20f49eedbe6fa4002ca1
Jacob Paul <jacob@jacobpaul.io>
2016-09-03 17:49:48 -0500

    Configure git

---
+++ git/aliases.zsh
@@ -0,0 +1,20 @@
+# Use `hub` as our git wrapper:
+#   http://defunkt.github.com/hub/
+hub_path=$(which hub)
+if (( $+commands[hub] ))
+then
+  alias git=$hub_path
+fi
+
+alias gl='git log --pretty=color'
+alias glg='git log --pretty=color --graph'
+alias gp='git push origin HEAD'
+alias gd='git diff'
+alias gc='git commit'
+alias gca='git commit -a'
+alias gco='git checkout'
+alias gcb='git copy-branch-name'
+alias gb='git branch'
+alias gs='git status -sb'
+alias gst='git status'
+alias gac='git add -A && git commit -m'
